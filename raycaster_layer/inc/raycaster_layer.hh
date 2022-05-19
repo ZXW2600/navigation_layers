59510dbeeb4b4c675dd6ccef92bda48b58b62062
zxw2600 <zhaoxinwei74@gmail.com>
2022-05-19 12:25:58 +0800

    add ray cast plugin

--- raycaster_layer/inc/raycaster_layer.hh
+++ raycaster_layer/inc/raycaster_layer.hh
@@ -12,32 +12,65 @@
 #ifndef __RAYCASTER_LAYER_HH__
 #define __RAYCASTER_LAYER_HH__
 
+#include <grid_map_msgs/GridMap.h>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_costmap_2d/grid_map_costmap_2d.hpp>
+
+#include <geometry_msgs/PointStamped.h>
+#include <nav_msgs/OccupancyGrid.h>
+
+#include <string>
+
 #include <costmap_2d/GenericPluginConfig.h>
 #include <costmap_2d/layer.h>
 #include <costmap_2d/layered_costmap.h>
 #include <dynamic_reconfigure/server.h>
 #include <ros/ros.h>
 
-namespace raycaster_layer_namespace {
+namespace raycaster_layer_namespace
+{
 
-class EnemiesLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D {
-public:
-  EnemiesLayer();
+  class EnemiesLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D
+  {
+  public:
+    EnemiesLayer();
 
-  virtual void onInitialize();
-  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw,
-                            double *min_x, double *min_y, double *max_x,
-                            double *max_y);
-  virtual void updateCosts(costmap_2d::Costmap2D &master_grid, int min_i,
-                           int min_j, int max_i, int max_j);
-  bool isDiscretized() { return true; }
+    virtual void onInitialize();
+    virtual void updateBounds(double robot_x, double robot_y, double robot_yaw,
+                              double *min_x, double *min_y, double *max_x,
+                              double *max_y);
+    virtual void updateCosts(costmap_2d::Costmap2D &master_grid, int min_i,
+                             int min_j, int max_i, int max_j);
+    bool isDiscretized() { return true; }
 
-  virtual void matchSize();
+    virtual void matchSize();
 
-private:
-  void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
-  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
-};
+  private:
+
+    void EnemiesPointCallback(const geometry_msgs::PointStamped::ConstPtr &msg);
+    void OccupancyGridCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg);
+
+    void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
+    dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+
+    ros::Subscriber map_sub;   // global reference map
+    ros::Subscriber point_sub; //
+
+    bool rolling_window_;
+
+    std::string global_frame_; ///< @brief The global frame for the costmap
+
+    std::string map_topic;
+    bool _static_map;
+
+    grid_map::Costmap2DConverter<grid_map::GridMap, grid_map::Costmap2DDirectTranslationTable> costmap2dConverter_;
+
+    grid_map::GridMap global_map;
+    bool global_map_inited = false;
+
+    float point_x = 0, point_y = 0;
+    bool point_inited = false;
+  };
 } // namespace raycaster_layer_namespace
 
 #endif // __RAYCASTER_LAYER_HH__
--- raycaster_layer/src/raycaster_layer.cpp
+++ raycaster_layer/src/raycaster_layer.cpp
@@ -1,12 +1,12 @@
 /**
  * @file raycaster_layer.cpp
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-05-14
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include "raycaster_layer.hh"
@@ -20,70 +20,140 @@ using costmap_2d::NO_INFORMATION;
 namespace raycaster_layer_namespace
 {
 
-EnemiesLayer::EnemiesLayer() {}
-
-void EnemiesLayer::onInitialize()
-{
-  ros::NodeHandle nh("~/" + name_);
-  current_ = true;
-  default_value_ = NO_INFORMATION;
-  matchSize();
-
-  dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
-  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
-      &EnemiesLayer::reconfigureCB, this, _1, _2);
-  dsrv_->setCallback(cb);
-}
-
-
-void EnemiesLayer::matchSize()
-{
-  Costmap2D* master = layered_costmap_->getCostmap();
-  resizeMap(master->getSizeInCellsX(), master->getSizeInCellsY(), master->getResolution(),
-            master->getOriginX(), master->getOriginY());
-}
-
-
-void EnemiesLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
-{
-  enabled_ = config.enabled;
-}
-
-void EnemiesLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x,
-                                           double* min_y, double* max_x, double* max_y)
-{
-  if (!enabled_)
-    return;
-
-  double mark_x = robot_x + cos(robot_yaw), mark_y = robot_y + sin(robot_yaw);
-  unsigned int mx;
-  unsigned int my;
-  if(worldToMap(mark_x, mark_y, mx, my)){
-    setCost(mx, my, LETHAL_OBSTACLE);
-  }
-  
-  *min_x = std::min(*min_x, mark_x);
-  *min_y = std::min(*min_y, mark_y);
-  *max_x = std::max(*max_x, mark_x);
-  *max_y = std::max(*max_y, mark_y);
-}
-
-void EnemiesLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
-                                          int max_j)
-{
-  if (!enabled_)
-    return;
-
-  for (int j = min_j; j < max_j; j++)
+  void EnemiesLayer::EnemiesPointCallback(const geometry_msgs::PointStamped::ConstPtr &msg)
   {
-    for (int i = min_i; i < max_i; i++)
+    point_inited = true;
+    point_y = msg->point.y;
+    point_x = msg->point.x;
+    ROS_INFO("get point: x %f y %f", point_x, point_y);
+  }
+
+  void EnemiesLayer::OccupancyGridCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
+  {
+    ROS_INFO("get map");
+
+    global_map_inited = true;
+    grid_map::GridMapRosConverter::fromOccupancyGrid(*msg, "elevation",
+                                                     global_map);
+    global_map["raycast"].resizeLike(global_map["elevation"]);
+    // shutdown the map subscrber if _static_map flag is on
+    if (_static_map)
     {
-      int index = getIndex(i, j);
-      if (costmap_[index] == NO_INFORMATION)
-        continue;
-      master_grid.setCost(i, j, costmap_[index]); 
+      ROS_INFO("Shutting down the map subscriber. _static_map flag is on");
+      map_sub.shutdown();
     }
   }
-}
+
+  EnemiesLayer::EnemiesLayer() : global_map({"elevation", "raycast"}) {}
+
+  void EnemiesLayer::onInitialize()
+  {
+    ros::NodeHandle nh("~/" + name_);
+    rolling_window_ = layered_costmap_->isRolling();
+
+    EnemiesLayer::matchSize();
+    current_ = true;
+
+    global_frame_ = layered_costmap_->getGlobalFrameID();
+
+    double transform_tolerance;
+    nh.param("transform_tolerance", transform_tolerance, 0.2);
+
+    if (!nh.getParam("map_topic", map_topic))
+    {
+      ROS_ERROR("~map_topic paramater not provieded");
+    }
+
+    if (!nh.getParam("static_map", _static_map))
+    {
+      ROS_INFO("~static_map paramater not provieded, set to true by default");
+      _static_map = true;
+    }
+
+    map_sub = nh.subscribe<nav_msgs::OccupancyGrid>(
+        map_topic, 1000, &EnemiesLayer::OccupancyGridCallback, this);
+
+    point_sub = nh.subscribe<geometry_msgs::PointStamped>(
+        "/clicked_point", 1000, &EnemiesLayer::EnemiesPointCallback, this);
+
+    dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
+
+    dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
+        &EnemiesLayer::reconfigureCB, this, _1, _2);
+    dsrv_->setCallback(cb);
+  }
+
+  void EnemiesLayer::matchSize()
+  {
+    Costmap2D *master = layered_costmap_->getCostmap();
+    resizeMap(master->getSizeInCellsX(), master->getSizeInCellsY(), master->getResolution(),
+              master->getOriginX(), master->getOriginY());
+  }
+
+  void EnemiesLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
+  {
+    enabled_ = config.enabled;
+  }
+
+  void EnemiesLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double *min_x,
+                                  double *min_y, double *max_x, double *max_y)
+  {
+    if (rolling_window_)
+      updateOrigin(robot_x - getSizeInMetersX() / 2, robot_y - getSizeInMetersY() / 2);
+    if (!enabled_)
+      return;
+
+    // update the global current status
+    current_ = true;
+  }
+
+  void EnemiesLayer::updateCosts(costmap_2d::Costmap2D &master_grid, int min_i, int min_j, int max_i,
+                                 int max_j)
+  {
+    if (global_map_inited && point_inited)
+    {
+      {
+        global_map.clear("raycast");
+        auto k = global_map.getResolution();
+
+        auto cols = global_map.getSize()(1) - 1;
+        auto rows = global_map.getSize()(0) - 1;
+        grid_map::Index center;
+        global_map.getIndex(grid_map::Position(point_x, point_y), center);
+
+        grid_map::Index corner[4] = {
+            grid_map::Index(0, 0), grid_map::Index(0, cols),
+            grid_map::Index(rows, cols), grid_map::Index(rows, 0)};
+        ROS_INFO("Running line iterator demo. col : %d row: %d k: %f", cols,
+                 rows, k);
+        for (size_t i = 0; i < 4; i++)
+        {
+          ROS_INFO(". to  x: %d y: %d k: %f", corner[(i + 1) % 4].x(),
+                   corner[(i + 1) % 4].y(), k);
+          for (grid_map::LineIterator iterator(global_map, corner[i],
+                                               corner[(i + 1) % 4]);
+               !iterator.isPastEnd(); ++iterator)
+          {
+
+            for (grid_map::LineIterator iterator_inner(global_map, center,
+                                                       (*iterator).transpose());
+                 !iterator_inner.isPastEnd(); ++iterator_inner)
+            {
+              if (global_map.at("elevation", *iterator_inner) < 10)
+              {
+                global_map.at("raycast", *iterator_inner) = 1;
+              }
+              else
+                break;
+            }
+          }
+        }
+      }
+
+      this->costmap2dConverter_.initializeFromGridMap(this->global_map, master_grid);
+      // Copy data.
+      this->costmap2dConverter_.setCostmap2DFromGridMap(this->global_map, "raycast", master_grid);
+    }
+  }
 
 } // end namespace
